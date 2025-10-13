f4e020cb4a57c3a3a64f1354f537556da14122ce
Maithili Mukherjee <86041684+maithilimukherjee@users.noreply.github.com>
2025-10-13 23:51:19 +0530

    implementing packages

---
+++ pack1/A.java
@@ -0,0 +1,9 @@
+package pack1;
+
+public class A
+{
+    public static void showA()
+    {
+        System.out.println("Hello from A. Pack 1.");
+    }
+}
---
+++ packDemo.java
@@ -0,0 +1,16 @@
+import pack1.A;
+
+public class packDemo
+{
+    public static void main(String[] args) {
+        
+    
+    A ob = new A();
+    ob.showA();
+
+}
+}
+
+/*
+ * Output: Hello from A. Pack 1.
+ */
