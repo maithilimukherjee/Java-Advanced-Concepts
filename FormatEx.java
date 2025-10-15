ad4da513d3cb8defa82b9a3503bbb0ee88b5c3ba
Maithili Mukherjee <86041684+maithilimukherjee@users.noreply.github.com>
2025-10-15 22:05:08 +0530

    number format exception handling

---
+++ FormatEx.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class FormatEx {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string but it must be a number: ");
+        String str = sc.nextLine();
+        try
+        {
+            int result = Integer.parseInt(str);
+            System.out.println("You entered the number: " + result);
+        }
+        catch(NumberFormatException e)
+        {
+            System.out.println("Error: Invalid input. Please enter a valid number.");
+        }
+        catch(Exception e)
+        {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
+}
+
+/*
+ * Enter a string but it must be a number: 457
+You entered the number: 457
+Enter a string but it must be a number: 67@3
+Error: Invalid input. Please enter a valid number.
+
+ */
